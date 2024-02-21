77aca346be33447c58b79633f52b43b6d2414ba7
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-02-21 22:44:05 +0000

    :sparkles: updated script configuration

--- docker/startup.sh
+++ docker/startup.sh
@@ -4,11 +4,30 @@
 
 # Initializing puppetServer
 echo "Initializing puppetServer"
-service puppetserver start
+/opt/puppetlabs/bin/puppetserver start &
 
-# Verifying puppetServer status
-echo "Verifying puppetServer status"
-service puppetserver status
+echo "verifing puppetServer status"
+# Function to check if the port is open
+check_port() {
+    nc -z localhost 8140
+    return $?
+}
+
+# Loop to check if the port is open
+while true; do
+    check_port
+    result=$?
+    if [ $result -eq 0 ]; then
+        echo "Porta 8140 está aberta!"
+        break
+    else
+        echo "Porta 8140 está fechada. Tentando novamente em 5 segundos..."
+        sleep 5
+    fi
+done
+
+# print the netstat ports: 
+netstat -alt
 
 # Keep the script running by tailing /dev/null
 tail -f /dev/null
