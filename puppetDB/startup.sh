98d733d5aae1b5af48459e195addbd318cd4a327
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-02-22 10:30:06 +0000

    :sparkles: added new version to main

---
+++ puppetDB/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Author: Arthur Cadore M. Barcella
+# Github: arthurcadore
+
+echo "Initializing puppetDB..."
+
+# TODO
+
+# 
+# /usr/sbin/puppetdb-ssl-setup
+
+# print the netstat ports:
+netstat -alt
+
+# Keep the script running by tailing /dev/null
+tail -f /dev/null
