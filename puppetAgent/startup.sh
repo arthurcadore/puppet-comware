f35e40c99b576d46040bdb62b5a481a64eb4508d
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-02-24 18:15:52 +0000

    :sparkles: added puppetAgent to test services

---
+++ puppetAgent/startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Author: Arthur Cadore M. Barcella
+# Github: arthurcadore
+
+# Initializing puppetServer
+#echo "Initializing puppetAgent"
+#service puppet start
+
+# Keep the script running by tailing /dev/null
+tail -f /dev/null
