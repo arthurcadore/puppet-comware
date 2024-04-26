42b492a706a567d40bae0c6d83eff8fcdf5f4217
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2024-04-26 19:41:28 -0300

    :sparkles: uptade main

--- app/startup.sh
+++ app/startup.sh
@@ -7,26 +7,6 @@ echo "Initializing puppetServer"
 /opt/puppetlabs/bin/puppetserver start &
 
 echo "verifing puppetServer status"
-
-# Function to check if the port is open
-check_port() {
-    nc -z localhost 8140
-    return $?
-}
-
-# Loop to check if the port is open
-while true; do
-    check_port
-    result=$?
-    if [ $result -eq 0 ]; then
-        echo "Porta 8140 está aberta!"
-        break
-    else
-        echo "Porta 8140 está fechada. Tentando novamente em 5 segundos..."
-        sleep 5
-    fi
-done
-
 # print the netstat ports: 
 netstat -alt
 
